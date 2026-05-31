116c1477590f852d4ab3d5910d4167deecdbc5ae
Nareg der Levonean <naregderlevonean@gmail.com>
2026-05-31 21:15:55 +0300

    OSK

--- src/.config/hypr/hyprland/lib/components/osk.lua
+++ src/.config/hypr/hyprland/lib/components/osk.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local process = "wvkbd-mobintl"
 local arguments = '-H 320 -L 320 -R 16 -alpha 148 '
                .. '--fn "Ubuntu Sans Mono Bold 10" '
                .. '--bg "#131313" --fg "#ededed" --fg-sp "#949494" '
@@ -8,10 +9,26 @@ local arguments = '-H 320 -L 320 -R 16 -alpha 148 '
                .. '--text "#ededed" --text-sp "#949494" '
                .. '-l simple,cyrillic,emoji'
 
+function M.start()
+    return function()
+        hl.exec_cmd("wvkbd-mobintl " .. arguments .. " &")
+    end
+end
+
+function M.stop()
+    return function()
+        hl.exec_cmd("pkill -34 -f " .. process)
+    end
+end
+
 function M.toggle()
     return function()
-        hl.exec_cmd("pkill -34 -f wvkbd-mobintl || wvkbd-mobintl " .. arguments .. " &")
+        hl.exec_cmd(string.format("pkill -34 -f %s || %s %s &", process, process, arguments))
     end
+end
+
+function M.status()
+    return helpers.process.status(process)
 end
 
 return M
