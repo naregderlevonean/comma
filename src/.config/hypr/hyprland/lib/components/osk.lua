11623e006071b38031504785903800f28f0c0b48
Nareg der Levonean <naregderlevonean@gmail.com>
2026-05-31 21:41:52 +0300

    Minor changes

--- src/.config/hypr/hyprland/lib/components/osk.lua
+++ src/.config/hypr/hyprland/lib/components/osk.lua
@@ -1,29 +1,29 @@
 local M = {}
 
 local process = "wvkbd-mobintl"
-local arguments = '-H 320 -L 320 -R 16 -alpha 148 '
-               .. '--fn "Ubuntu Sans Mono Bold 10" '
-               .. '--bg "#131313" --fg "#ededed" --fg-sp "#949494" '
-               .. '--press "#1a1a1a" --press-sp "#1a1a1a" '
-               .. '--swipe "#212121" --swipe-sp "#292929" '
-               .. '--text "#ededed" --text-sp "#949494" '
-               .. '-l simple,cyrillic,emoji'
+local arguments = "-H 320 -L 320 -R 16 -alpha 148"
+    .. " --fn 'Ubuntu Sans Mono Bold 10'"
+    .. " --bg '#131313' --fg '#ededed' --fg-sp '#949494'"
+    .. " --press '#1a1a1a' --press-sp '#1a1a1a'"
+    .. " --swipe '#212121' --swipe-sp '#292929'"
+    .. " --text '#ededed' --text-sp '#949494'"
+    .. " -l simple,cyrillic,emoji"
 
 function M.start()
     return function()
-        hl.exec_cmd("wvkbd-mobintl " .. arguments .. " &")
+        helpers.process.start(string.format("%s %s", process, arguments))
     end
 end
 
 function M.stop()
     return function()
-        hl.exec_cmd("pkill -34 -f " .. process)
+        helpers.process.stop(process)
     end
 end
 
 function M.toggle()
     return function()
-        hl.exec_cmd(string.format("pkill -34 -f %s || %s %s &", process, process, arguments))
+        helpers.process.toggle(string.format("%s %s", process, arguments))
     end
 end
 
