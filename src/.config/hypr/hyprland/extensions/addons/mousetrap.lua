5822f6bd19733eb25b1474d72b01a160b900d267
Nareg der Levonean <naregderlevonean@gmail.com>
2026-06-01 21:48:50 +0300

    Minor changes

--- src/.config/hypr/hyprland/extensions/addons/mousetrap.lua
+++ src/.config/hypr/hyprland/extensions/addons/mousetrap.lua
@@ -5,11 +5,17 @@ local mousetrap = require("hyprland.extensions.addons.mousetrap.init").setup({
 })
 
 mousetrap.bind("top-left", components.walker.toggle(), { delay = 300 })
-mousetrap.bind("bottom-left", actions.specialworkspace.toggle(""), { delay = 300 })
+
+mousetrap.bind("bottom-left", actions.specialworkspace.toggle("", {
+        start = function() components.waybar.hide()() end,
+        stop  = function() components.waybar.show()() end
+    }), { delay = 300 })
+
 mousetrap.bind("bottom-right", components.walker.toggle({ provider = "windows" }), { delay = 100 })
+
 mousetrap.bind("top-right", function() hl.dsp.exec_cmd("hyprlock") end, { delay = 1000 })
 
-mousetrap.bind("top", actions.scoped.workspace(actions.workspace.focus("prev")), { flick = 100 })
-mousetrap.bind("bottom", actions.scoped.workspace(actions.workspace.focus("next")), { flick = 100 })
+mousetrap.bind("top", actions.scoped.workspace(actions.workspace.focus("prev")), { flick = 160 })
+mousetrap.bind("bottom", actions.scoped.workspace(actions.workspace.focus("next")), { flick = 160 })
 
 addons.mousetrap = mousetrap
