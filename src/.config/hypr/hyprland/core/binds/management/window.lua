de2656fdf1c8b414e060111b3c00d4fa378b3a50
Nareg der Levonean <naregderlevonean@gmail.com>
2026-06-01 23:49:59 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/binds/management/window.lua
+++ src/.config/hypr/hyprland/core/binds/management/window.lua
@@ -1,4 +1,13 @@
-hl.bind("SUPER + CTRL + Escape", actions.scoped.workspace(actions.window.float()))
-hl.bind("SUPER + CTRL + Grave", actions.scoped.workspace(hl.dsp.window.pin()))
-hl.bind("SUPER + CTRL + Return", actions.scoped.workspace(hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" })))
+hl.bind(
+    "SUPER + CTRL + Escape",
+    actions.scoped.workspace(actions.window.float()))
+
+hl.bind(
+    "SUPER + CTRL + Grave",
+    actions.scoped.workspace(hl.dsp.window.pin()))
+
+hl.bind("SUPER + CTRL + Return",
+    actions.scoped.workspace(hl.dsp.window.fullscreen(
+        { mode = "fullscreen", action = "toggle" }
+    )))
 
