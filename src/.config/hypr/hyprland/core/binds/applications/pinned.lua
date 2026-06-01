e5ad05a50d0da80b6b3abb77fe15089760f84032
Nareg der Levonean <naregderlevonean@gmail.com>
2026-06-01 21:51:17 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/binds/applications/pinned.lua
+++ src/.config/hypr/hyprland/core/binds/applications/pinned.lua
@@ -1,7 +1,14 @@
-hl.bind("SUPER + F4", hl.dsp.exec_cmd("waypaper"))
-hl.bind("SUPER + F5", hl.dsp.exec_cmd("gnome-calculator"))
-hl.bind("SUPER + F6", hl.dsp.exec_cmd("dialect"))
-hl.bind("SUPER + F7", hl.dsp.exec_cmd("gnome-characters"))
-hl.bind("SUPER + F8", hl.dsp.exec_cmd("gradia --screenshot"))
-hl.bind("SUPER + F9", hl.dsp.exec_cmd("eyedropper"))
+local function launch(application)
+    return actions.scoped.workspace(
+        hl.dsp.exec_cmd(application),
+        { exclude = { "special:stylus", "special:radio" } }
+    )
+end
+
+hl.bind("SUPER + F4", launch("waypaper"))
+hl.bind("SUPER + F5", launch("gnome-calculator"))
+hl.bind("SUPER + F6", launch("dialect"))
+hl.bind("SUPER + F7", launch("gnome-characters"))
+hl.bind("SUPER + F8", launch("gradia --screenshot"))
+hl.bind("SUPER + F9", launch("eyedropper"))
 
