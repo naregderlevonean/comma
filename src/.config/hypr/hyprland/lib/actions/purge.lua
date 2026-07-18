db0cc2d49cf876b5a55f0b0fce0f28e67ebe0e3f
Nareg der Levonean <naregderlevonean@gmail.com>
2026-07-18 03:58:42 +0300

    Fix

--- src/.config/hypr/hyprland/lib/actions/purge.lua
+++ src/.config/hypr/hyprland/lib/actions/purge.lua
@@ -1,14 +1,18 @@
 local M = {}
 
 function M.init()
-	local list = hl.get_windows()
-	if not list then
-		return
-	end
+	return function()
+		local list = hl.get_windows()
+		if not list then
+			return
+		end
 
-	for _, item in ipairs(list) do
-		if item.address then
-			hl.dispatch(hl.dsp.window.close({ window = "address:" .. item.address }))
+		for _, item in ipairs(list) do
+			if item.address then
+				if not string.match(item.workspace.name, "^special:") then
+					hl.dispatch(hl.dsp.window.close({ window = "address:" .. item.address }))
+				end
+			end
 		end
 	end
 end
