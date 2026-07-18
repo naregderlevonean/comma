4bbeec9122a8e7d52def932327e4576dfd82d7f7
Nareg der Levonean <naregderlevonean@gmail.com>
2026-07-18 14:17:57 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/binds/layouts/scrolling.lua
+++ src/.config/hypr/hyprland/core/binds/layouts/scrolling.lua
@@ -14,3 +14,14 @@ hl.bind("SUPER + CTRL + 3", hl.dsp.layout("colresize 0.333333"))
 hl.bind("SUPER + SHIFT + CTRL + 1", hl.dsp.layout("colresize all 0.9"))
 hl.bind("SUPER + SHIFT + CTRL + 2", hl.dsp.layout("colresize all 0.5"))
 hl.bind("SUPER + SHIFT + CTRL + 3", hl.dsp.layout("colresize all 0.333333"))
+
+hl.bind("SUPER + CTRL + C", function()
+	local state = tonumber(hl.get_config("scrolling:focus_fit_method"))
+	local next_state = (state == 1) and 0 or 1
+
+	hl.config({
+		scrolling = {
+			focus_fit_method = next_state,
+		},
+	})
+end)
