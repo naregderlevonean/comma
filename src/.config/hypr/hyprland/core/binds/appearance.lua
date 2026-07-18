fc7dcdab5f5b26620cac7495beb45a54d4ccff2f
Nareg der Levonean <naregderlevonean@gmail.com>
2026-07-18 06:13:24 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/binds/appearance.lua
+++ src/.config/hypr/hyprland/core/binds/appearance.lua
@@ -1,2 +1,3 @@
+require("hyprland.core.binds.appearance.decoration")
 require("hyprland.core.binds.appearance.theme")
 require("hyprland.core.binds.appearance.zoom")
---
+++ src/.config/hypr/hyprland/core/binds/appearance/decoration.lua
@@ -0,0 +1,32 @@
+hl.bind("SUPER + CTRL + B", function()
+	local state = hl.get_config("decoration.blur.enabled")
+	hl.config({
+		decoration = {
+			blur = {
+				enabled = not state,
+			},
+		},
+	})
+end)
+
+hl.bind("SUPER + CTRL + O", function()
+	local state = hl.get_config("decoration.active_opacity")
+
+	if state and state < 1.00 then
+		hl.config({
+			decoration = {
+				active_opacity = 1.00,
+				fullscreen_opacity = 1.00,
+				inactive_opacity = 1.00,
+			},
+		})
+	else
+		hl.config({
+			decoration = {
+				active_opacity = 0.90,
+				fullscreen_opacity = 0.90,
+				inactive_opacity = 0.80,
+			},
+		})
+	end
+end)
