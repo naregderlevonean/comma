7e0ac73e16eaaa7d0baf7727e0d88b85d5b4c92d
Nareg der Levonean <naregderlevonean@gmail.com>
2026-07-31 17:55:56 +0300

    Minor changes

--- src/.config/hypr/hyprland/extensions/addons.lua
+++ src/.config/hypr/hyprland/extensions/addons.lua
@@ -1,3 +1,2 @@
 require("hyprland.extensions.addons.mousetrap")
-require("hyprland.extensions.addons.orbit")
 require("hyprland.extensions.addons.spin")
--- src/.config/hypr/hyprland/extensions/addons/orbit.lua
+++
@@ -1,7 +0,0 @@
-local orbit = require("hyprland.extensions.addons.orbit.init").setup({
-	workspace_count = 9,
-})
-
-orbit.start()
-
-addons.orbit = orbit
