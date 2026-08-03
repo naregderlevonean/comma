e4dbe1d3f92c2f92f0d06fb7b03c21774a952daa
Nareg der Levonean <naregderlevonean@gmail.com>
2026-08-03 12:47:49 +0300

    Minor changes

--- src/.config/hypr/hyprland/extensions/addons/mousetrap.lua
+++ src/.config/hypr/hyprland/extensions/addons/mousetrap.lua
@@ -1,7 +1,4 @@
 local addon = require("hyprland.extensions.addons.mousetrap.init").setup({
-	trigger = {
-		fire = false,
-	},
 	geometry = {
 		default = {
 			corner = 2,
@@ -11,6 +8,16 @@ local addon = require("hyprland.extensions.addons.mousetrap.init").setup({
 })
 
 -- Workspace
+
+addon.bind("right", actions.scoped.workspace(actions.workspace.focus("next")), {
+	direction = "up",
+	distance = 300,
+})
+
+addon.bind("right", actions.scoped.workspace(actions.workspace.focus("prev")), {
+	direction = "down",
+	distance = 300,
+})
 
 addon.bind("right", function()
 	hl.bind("mouse_up", actions.scoped.workspace(actions.workspace.focus("prev")))
@@ -27,6 +34,16 @@ end, {
 })
 
 -- Window
+
+addon.bind("bottom", actions.window.focus("l"), {
+	direction = "right",
+	distance = 300,
+})
+
+addon.bind("bottom", actions.window.focus("r"), {
+	direction = "left",
+	distance = 300,
+})
 
 addon.bind("left", actions.window.fit(), {
 	delay = 300,
