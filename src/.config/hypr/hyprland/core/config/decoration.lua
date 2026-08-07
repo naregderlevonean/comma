3a7c8e0cfccac25f9d81667dfb885c684b82aa26
Nareg der Levonean <naregderlevonean@gmail.com>
2026-08-07 20:37:14 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/config/decoration.lua
+++ src/.config/hypr/hyprland/core/config/decoration.lua
@@ -2,10 +2,10 @@ hl.config({
 	decoration = {
 		active_opacity = 0.96,
 		border_part_of_window = false,
-		dim_around = 0.92,
+		dim_around = 0.88,
 		dim_inactive = false,
 		dim_modal = true,
-		dim_special = 0.16,
+		dim_special = 0.48,
 		dim_strength = 0.64,
 		fullscreen_opacity = 0.96,
 		inactive_opacity = 0.84,
