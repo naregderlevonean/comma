a6938f759b7251acba96538c32b7fd31f7efb083
Nareg der Levonean <naregderlevonean@gmail.com>
2026-08-13 08:31:35 +0300

    Mintor changes

--- src/.config/hypr/hyprland/visual/animations/presets/elastic/windows.lua
+++ src/.config/hypr/hyprland/visual/animations/presets/elastic/windows.lua
@@ -1,4 +1,4 @@
-hl.animation({ leaf = "windows", enabled = true, speed = 1, spring = "soft5" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 1, spring = "soft5", style = "slide bottom" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1, spring = "stable9", style = "slide bottom" })
-hl.animation({ leaf = "windowsMove", enabled = true, speed = 1, spring = "soft5" })
+hl.animation({ leaf = "windows", enabled = true, speed = 1, spring = "soft6" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 1, spring = "soft6", style = "slide bottom" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1, spring = "natural6", style = "slide bottom" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = 1, spring = "soft6" })
