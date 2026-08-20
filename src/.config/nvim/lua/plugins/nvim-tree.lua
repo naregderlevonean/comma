027980c6cb5d26b8b9583b93894164fcec10f431
Nareg der Levonean <naregderlevonean@gmail.com>
2026-08-20 19:04:40 +0300

    Minor changes

--- src/.config/nvim/lua/plugins/nvim-tree.lua
+++ src/.config/nvim/lua/plugins/nvim-tree.lua
@@ -71,11 +71,11 @@ return {
 				glyphs = {
 					folder = {
 						default = "",
-						open = "",
+						open = "",
 						empty = "",
-						empty_open = "",
+						empty_open = "",
 						symlink = "",
-						symlink_open = "",
+						symlink_open = "",
 					},
 				},
 				git_placement = "before",
