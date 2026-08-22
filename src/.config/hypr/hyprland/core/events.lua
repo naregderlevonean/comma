5700e051a6c76e890588445a45a1669a149ca23e
Nareg der Levonean <naregderlevonean@gmail.com>
2026-08-22 04:01:52 +0300

    Minor changes

--- src/.config/hypr/hyprland/core/events.lua
+++ src/.config/hypr/hyprland/core/events.lua
@@ -1,2 +1,5 @@
 hl.on("workspace.active", components.waybar.refresh())
 hl.on("monitor.layout_changed", components.waybar.refresh())
+
+hl.on("workspace.active", components.walker.stop())
+hl.on("workspace.special_active", components.walker.stop())
